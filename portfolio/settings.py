3e95b796c093955dd52798b0b941d2eba4018345
parallel9973 <parallel9973@gmail.com>
2019-10-26 00:24:22 +0900

    heroku edited

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'sauron'
 ]
 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'portfolio.urls'
@@ -127,4 +129,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # 로그인 이후 경로 수정
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
+
+# settings.py # Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
